681fc0a41e3e2e66fc1291ff8b6c3e4d01c5d6fb
Neo-Machina <andreistatache1997@gmail.com>
2022-07-26 17:01:23 +0200

    header completed

--- index.php
+++ index.php
@@ -5,10 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-AJAX-DISCHI</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <!-- HEADER -->
+    <header>
+        <div class="flex_img">
+            <img src="img/logo_spotify.png" alt="spotify_logo">
+        </div>
+    </header>
 </body>
 </html>
