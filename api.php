545b7417fa825e83ae099682dbdb90f1bd8907b1
Neo-Machina <andreistatache1997@gmail.com>
2022-07-27 10:27:36 +0200

    bonus completed

--- api.php
+++ api.php
@@ -1,6 +1,17 @@
 <?php
     require __DIR__ . '/database.php';
+    $genreParameter = $_GET['genre'];
+    $filteredArray = [];
     
-    header('Content-Type: application/json');
-    echo json_encode($discsArray);
+    if($genreParameter) {
+        $filteredArray = array_filter($discsArray, function ($disc) use ($genreParameter) {
+            return (strtolower((string)$disc['genre']) == $genreParameter);
+        });
+
+        header('Content-Type: application/json');
+        echo json_encode($filteredArray);
+    } else {
+        header('Content-Type: application/json');
+        echo json_encode($discsArray);
+    }
 ?>
--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 var app = new Vue({
     el: '#root',
     data: {
-      discs: []
+      discs: [],
+      genreArray:[
+        'Rock',
+        'Pop',
+        'Jazz',
+        'Metal'
+      ]
+    },
+    methods: {
+      changeGenre(musicGenre) {
+        axios.get('http://localhost:8888/php-ajax-dischi/api.php?genre=' + musicGenre.target.value.toLowerCase()) 
+        .then((response) => {
+            this.discs = response.data;
+        })
+      }
     },
     mounted() {
         axios.get('http://localhost:8888/php-ajax-dischi/api.php') 
