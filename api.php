745c32a1e3a62cf8dcf8505c4cb06a09b30ab32f
Neo-Machina <andreistatache1997@gmail.com>
2022-07-28 09:30:11 +0200

    select different method

--- api.php
+++ api.php
@@ -1,17 +1,19 @@
 <?php
     require __DIR__ . '/database.php';
-    $genreParameter = $_GET['genre'];
-    $filteredArray = [];
     
-    if($genreParameter) {
-        $filteredArray = array_filter($discsArray, function ($disc) use ($genreParameter) {
-            return (strtolower((string)$disc['genre']) == $genreParameter);
-        });
-
-        header('Content-Type: application/json');
-        echo json_encode($filteredArray);
-    } else {
+    if(empty($_GET['genre']) || $_GET['genre'] === 'all') {
         header('Content-Type: application/json');
         echo json_encode($discsArray);
+    } else {
+        $filteredArray = [];
+    
+        foreach($discsArray as $disc) {
+            if($disc['genre'] === $_GET['genre']) {
+                $filteredArray[] = $disc;
+            }
+        }
+    
+        header('Content-Type: application/json');
+        echo json_encode($filteredArray);
     }
 ?>
--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-var app = new Vue({
-    el: '#root',
-    data: {
-      discs: [],
-      genreArray:[
-        'Rock',
-        'Pop',
-        'Jazz',
-        'Metal'
-      ]
-    },
-    methods: {
-      changeGenre(musicGenre) {
-        axios.get('http://localhost:8888/php-ajax-dischi/api.php?genre=' + musicGenre.target.value.toLowerCase()) 
-        .then((response) => {
-            this.discs = response.data;
-        })
+var app = new Vue(
+  {
+      el: '#root',
+      data: {
+          discs: [],
+          userGenre: 'all'
+      },
+      methods: {
+          getDiscsFromAPI() {
+              axios.get('http://localhost:8888/php-ajax-dischi/api.php',
+              {
+                  params: {
+                      genre: this.userGenre
+                  }
+              }
+              )
+              .then((response) => {
+                  this.discs = response.data;
+              });
+          }
+      },
+      mounted() {
+          this.getDiscsFromAPI();
       }
-    },
-    mounted() {
-        axios.get('http://localhost:8888/php-ajax-dischi/api.php') 
-        .then((response) => {
-            this.discs = response.data;
-        })
-    }
-  })
+  }
+);
